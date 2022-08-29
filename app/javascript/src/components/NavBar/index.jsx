24378fd3b741e49e1a82fce97574da0b7fc963e9
Eshaan <eshaan.s@bigbinary.com>
2022-08-29 18:01:40 +0530

    Added login feature

--- app/javascript/src/components/NavBar/index.jsx
+++ app/javascript/src/components/NavBar/index.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
+import { getFromLocalStorage } from "utils/storage";
+
 import NavItem from "./NavItem";
 
-const NavBar = () => (
+const NavBar = () => {
+  const userName = getFromLocalStorage("authUserName");
+
   <nav className="shadow bg-white">
     <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
       <div className="flex h-16 justify-between">
@@ -23,9 +27,18 @@ const NavBar = () => (
             LogOut
           </a>
         </div>
+        <div className="flex items-center justify-end gap-x-4">
+          <span
+            className="font-regular transition focus:outline-none inline-flex items-center border-b-2 border-transparent px-2
+              pt-1 text-sm leading-5 text-bb-gray-600 text-opacity-50 duration-150 ease-in-out
+              focus:text-bb-gray-700"
+          >
+            {userName}
+          </span>
+        </div>
       </div>
     </div>
-  </nav>
-);
+  </nav>;
+};
 
 export default NavBar;
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   MAX_NAME_LENGTH = 255
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
   MAX_EMAIL_LENGTH = 255
-  has_secure_password
   has_many :assigned_tasks, foreign_key: :assigned_user_id, class_name: "Task"
+  has_secure_password
+  has_secure_token :authentication_token
 
   validates :email, presence: true,
     uniqueness: { case_sensitive: false },
